692e25506f37e6c598f6f9a21d723a08b5cc336c
Chloe <56194759+Hammer86gn@users.noreply.github.com>
2022-11-16 19:45:39 -0500

    feat: Input This needs windowing to actually work...

---
+++ include/azalea/core/Input.hpp
@@ -0,0 +1,20 @@
+#ifndef AZALEA_KEY_HPP
+#define AZALEA_KEY_HPP
+
+#include <azalea/core/Key.hpp>
+#include <azalea/core/Mouse.hpp>
+
+namespace azalea {
+
+    bool isKeyPressed(KeyCode keyCode);
+    bool isKeyDown(KeyCode keyCode);
+    bool isKeyReleased(KeyCode keyCode);
+
+    bool isMouseButtonPressed(MouseButton mouseButton);
+    bool isMouseButtonDown(MouseButton mouseButton);
+    bool isMouseButtonReleased(MouseButton mouseButton);
+
+}// namespace azalea
+
+
+#endif// AZALEA_KEY_HPP
---
+++ src/core/Input.cpp
@@ -0,0 +1,45 @@
+#include <azalea/core/Input.hpp>
+#include <GLFW/glfw3.h>
+
+bool azalea::isKeyPressed(azalea::KeyCode keyCode)
+{
+    // TODO(Chloe): Fix me
+    GLFWwindow* window;
+    return glfwGetKey(window, keyCode) == GLFW_PRESS;
+}
+
+bool azalea::isKeyDown( azalea::KeyCode keyCode )
+{
+    // TODO(Chloe): Fix me
+    GLFWwindow* window;
+    return glfwGetKey(window, keyCode) == GLFW_REPEAT;
+}
+
+bool azalea::isKeyReleased( azalea::KeyCode keyCode )
+{
+    // TODO(Chloe): Fix me
+    GLFWwindow* window;
+    return glfwGetKey(window, keyCode) == GLFW_RELEASE;
+}
+
+
+bool azalea::isMouseButtonPressed( azalea::MouseButton mouseButton )
+{
+    // TODO(Chloe): Fix me
+    GLFWwindow* window;
+    return glfwGetMouseButton(window, mouseButton) == GLFW_RELEASE;
+}
+
+bool azalea::isMouseButtonDown( azalea::MouseButton mouseButton )
+{
+    // TODO(Chloe): Fix me
+    GLFWwindow* window;
+    return glfwGetMouseButton(window, mouseButton) == GLFW_RELEASE;
+}
+
+bool azalea::isMouseButtonReleased( azalea::MouseButton mouseButton )
+{
+    // TODO(Chloe): Fix me
+    GLFWwindow* window;
+    return glfwGetMouseButton(window, mouseButton) == GLFW_RELEASE;
+}
