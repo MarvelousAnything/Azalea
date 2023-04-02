c940aacfdb85a16f4f738c350e530539fc48143d
MarvelousAnything <marvelousanything@gmail.com>
2023-04-01 23:20:14 -0700

    [Add] __APPLE__ as a valid platform.

--- AzaleaEngine/include/Azalea/Platform/Platform.hpp
+++ AzaleaEngine/include/Azalea/Platform/Platform.hpp
@@ -1,12 +1,14 @@
 #ifndef AZALEA_PLATFORM_HPP
 #define AZALEA_PLATFORM_HPP
 
-#if not defined AZALEA_PLATFORM_WINDOWS || not defined AZALEA_PLATFORM_LINUX
+#if not defined AZALEA_PLATFORM_WINDOWS || not defined AZALEA_PLATFORM_LINUX || not defined AZALEA_PLATFORM_MACOS
 
 #if defined WIN32 || defined WIN64
 #define AZALEA_PLATFORM_WINDOWS
 #elif defined __linux__
 #define AZALEA_PLATFORM_LINUX
+#elif defined __APPLE__
+#define AZALEA_PLATFORM_MACOS
 #else
 #error "Unknown or Unsupported Platform"
 #endif// If defined WIN32 ...
